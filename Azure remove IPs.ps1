44dee6d9bd400eecbd79deeee166f91f63bfd0b8
nitbhatt0 <91825953+nitbhatt0@users.noreply.github.com>
2022-09-05 14:52:21 +0530

    Add files via upload
    Use Azure Resource Graph query to get list of all IPs that are unattached and export the data into an excel file 'export_data.csv' >>
    
    resources
    | where type =~ 'Microsoft.Network/publicIPAddresses'
    | where isempty(properties.ipConfiguration)

---
+++ Azure remove IPs.ps1
@@ -0,0 +1,9 @@
+﻿Set-AzContext -Subscription ""
+
+$ips= Import-Csv -Path C:\Users\nitbhatt\Downloads\export_data.csv
+
+foreach ($item in $ips)
+{
+Remove-AzPublicIpAddress -Name $item.IPName -ResourceGroupName $item.PIPRG -Force
+}
+
