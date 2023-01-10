599d292b382032b59440b39dbfeb20f202af3b2a
nitbhatt0 <91825953+nitbhatt0@users.noreply.github.com>
2023-01-10 09:36:01 +0530

    Update Change Backup Policy.ps1

--- Change Backup Policy.ps1
+++ Change Backup Policy.ps1
@@ -1,58 +1,47 @@
 ﻿#Connect-AzAccount
-$rg= "Nit-rg1"
-$rsv= "rsv-1"
+$rg= "<ResourceGroup Name>"
+$rsv= "<Name of the Recovery service vault>"
 $vms = Get-AzVM
 $backupVaults = Get-AzRecoveryServicesVault
 $outputs=@();
 $i=0
-$targetpolicy= "pol2"
+$targetpolicy= "<Target Backup Policy Name"
 
 Get-AzRecoveryServicesVault  -Name "$rsv" | Set-AzRecoveryServicesVaultContext
 
 #VM protection Status
  
  foreach ($vm in $vms) 
-    {
+{
         $recoveryVaultInfo = Get-AzRecoveryServicesBackupStatus -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -Type 'AzureVM'
 
  if ($recoveryVaultInfo.BackedUp -eq $true)
         {
-            
-           #Write-Host "$($vm.Name)"
-            
-			$vmBackupVault = $backupVaults | Where-Object {$_.ID -eq $recoveryVaultInfo.VaultId}
-
+            $vmBackupVault = $backupVaults | Where-Object {$_.ID -eq $recoveryVaultInfo.VaultId}
             $container = Get-AzRecoveryServicesBackupContainer -ContainerType AzureVM -VaultId $vmBackupVault.ID -FriendlyName $vm.Name #-Status "Registered" 
             $backupItem = Get-AzRecoveryServicesBackupItem -Container $container -WorkloadType AzureVM -VaultId $vmBackupVault.ID
 
-           # $vmBackupVault.Name
-           # $backupItem.ProtectionPolicyName
-
-              $obj = [pscustomobject]@{ 
+            $obj = [pscustomobject]@{ 
                   "VMName" = ($vm.Name)   
                   "ARSVaultName" = ($vmBackupVault.Name)    
                   "BackupPolicyName"=($backupItem.ProtectionPolicyName)    
-                                      }    
+                                     }    
               $outputs+= $obj
 
         } 
 else 
         {
-            #Write-Host "$($vm.Name) - BackedUp : No" -BackgroundColor DarkRed
+            Write-Host "$($vm.Name) - BackedUp : No" -BackgroundColor DarkRed
             $vmBackupVault = $null
             $container =  $null
             $backupItem =  $null
         } 
-
-
 }
 
 #ASRVault remains the same, only Backup policy is changed.
 $TargetPol1 = Get-AzRecoveryServicesBackupProtectionPolicy -Name $targetpolicy -VaultId $Vault.ID
 foreach ($output in $outputs)
-    {
+{
     $anotherBkpItem = Get-AzRecoveryServicesBackupItem -WorkloadType AzureVM -BackupManagementType AzureVM -Name $output.VMName -VaultId $Vault.ID
     Enable-AzRecoveryServicesBackupProtection -Item $anotherBkpItem -Policy $TargetPol1 -VaultId $Vault.ID
-    #$anotherBkpItem = $null
-
-     }
+}
