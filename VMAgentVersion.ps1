df093773023e2c6d249ec77a92a57d1fe5e3474d
nitbhatt0 <91825953+nitbhatt0@users.noreply.github.com>
2023-02-14 20:57:43 +0530

    Add files via upload
    Pull details of current VM Agent version across subscriptions

---
+++ VMAgentVersion.ps1
@@ -0,0 +1,24 @@
+﻿#Connect-AzAccount
+
+$subs=Get-AzSubscription | Sort Name | Select Name
+foreach ($sub in $subs)
+{
+Set-AzContext -SubscriptionName $sub.Name | Out-null
+#$subname= (Get-AzSubscription -SubscriptionId $subid).Name
+$vms = Get-AzVM
+Write-Host "`n Subscription Name $($sub.Name)"
+foreach ($vm in $vms) 
+    {
+        $azvm = Get-AzVM -Name $vm
+        #$subid = ($azvm.Id -split '/')[2]
+        #$subname= (Get-AzSubscription -SubscriptionId $subid).Name
+        $vmStatus = Invoke-AzRestMethod -Path ('{0}/instanceView?api-version=2020-06-01' -f $vm.Id) -Method GET | 
+                            Select-Object -ExpandProperty Content | ConvertFrom-Json
+          
+               if ($vmStatus.vmAgent.statuses.displayStatus -ne "Ready") {
+                Write-Host "$($vm.Name) is missing an agent"
+            } else {
+                Write-Host "$($vm.Name) is running agent version $($vmStatus.vmAgent.vmAgentVersion)"
+            }
+      }
+}
